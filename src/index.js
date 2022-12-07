9a939cfc7a50a14f6b8d52e83c86d8818ad8be8a
Naseef Mashood <107404776+nosfeeferatu@users.noreply.github.com>
2022-12-08 02:43:59 +0530

    Added footer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import "./styles.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -9,5 +10,22 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
+    <footer>
+      <div className="policy">
+        This app doesn't store any of your data. We don't have a database for
+        that.
+      </div>
+      <ul>
+        <li>
+          <a href="https://github.com/nosfeeferatu">GitHub</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/naseefmashood/">LinkedIn</a>
+        </li>
+        <li>
+          <a href="https://twitter.com/nosfeeferatu">Twitter</a>
+        </li>
+      </ul>
+    </footer>
   </StrictMode>
 );
